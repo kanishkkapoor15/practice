22ece584baaaba7146d24db3f3a14364d7c60f11
kanishkkapoor15 <71957724+kanishkkapoor15@users.noreply.github.com>
2022-01-09 05:47:22 -0500

    Add files via upload

---
+++ loginform.js
@@ -0,0 +1,22 @@
+<script>
+
+function submit_form(){
+alert("logged in!");
+}
+
+function create(){
+window.location="signup.html";
+}
+
+function validate(){
+var pass=document.getElementById("pass").value;
+if(pass =="abcd"){
+alert("correct password");
+}
+else{
+alert("incorrect password");
+}
+}
+
+
+</script>
